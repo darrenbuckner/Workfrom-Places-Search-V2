0607d3ffa4e4472f21426e6747323aef7376e1d5
Darren Buckner <darren@darrenbuckner.com>
2024-10-20 09:34:53 -0700

    added icon for user's location and a link to get directions in the map popup

--- src/NearbyPlacesMap.js
+++ src/NearbyPlacesMap.js
@@ -1,52 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
-// Fix for default marker icon
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+import { Navigation } from 'lucide-react';
 
 const NearbyPlacesMap = ({ places, userLocation }) => {
-  const [activePlace, setActivePlace] = useState(null);
+  const defaultPosition = [userLocation.latitude, userLocation.longitude];
+
+  const userIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  const placeIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  const getGoogleMapsUrl = (address) => {
+    return `https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(address)}`;
+  };
 
   return (
-    <MapContainer center={[userLocation.latitude, userLocation.longitude]} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={defaultPosition} zoom={13} style={{ height: '500px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {places.map(place => (
+      <Marker position={defaultPosition} icon={userIcon}>
+        <Popup>Your location</Popup>
+      </Marker>
+      {places.map((place) => (
         <Marker
           key={place.ID}
           position={[place.latitude, place.longitude]}
-          eventHandlers={{
-            mouseover: () => setActivePlace(place),
-            mouseout: () => setActivePlace(null),
-          }}
+          icon={placeIcon}
         >
           <Popup>
             <div>
-              <h3>{place.title}</h3>
+              <h3 className="font-bold">{place.title}</h3>
+              <p>{place.street}, {place.city}, {place.postal}</p>
               <p>Distance: {place.distance} miles</p>
-              {place.download && <p>WiFi Speed: {Math.round(place.download)} Mbps</p>}
-              <p>Noise Level: {place.mappedNoise}</p>
+              {place.download && (
+                <p>WiFi Speed: {Math.round(place.download)} Mbps</p>
+              )}
+              <p>Background Noise: {place.mappedNoise}</p>
+              <a
+                href={getGoogleMapsUrl(`${place.street}, ${place.city}, ${place.postal}`)}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:text-blue-700 flex items-center mt-2"
+              >
+                <Navigation size={16} className="mr-1" />
+                Get Directions
+              </a>
             </div>
           </Popup>
         </Marker>
       ))}
-      {activePlace && (
-        <div className="absolute bottom-0 left-0 bg-white p-2 m-2 rounded shadow z-1000">
-          <h3>{activePlace.title}</h3>
-          <p>Distance: {activePlace.distance} miles</p>
-          {activePlace.download && <p>WiFi Speed: {Math.round(activePlace.download)} Mbps</p>}
-          <p>Noise Level: {activePlace.mappedNoise}</p>
-        </div>
-      )}
     </MapContainer>
   );
 };
